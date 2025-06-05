1d7a86cf67e2dd942908328544d1e083a735491c
RoMANzhula <roman1985al@ukr.net>
2025-06-06 00:17:29 +0300

    Add UserResponse - user-service

---
+++ user-service/src/main/java/org/romanzhula/user_service/dto/UserResponse.java
@@ -0,0 +1,15 @@
+package org.romanzhula.user_service.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserResponse {
+
+    private String name;
+    private String email;
+
+}
