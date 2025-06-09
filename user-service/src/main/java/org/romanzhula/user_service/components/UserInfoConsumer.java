4316543be13a1f691bf522cbc90688543ac1843b
RoMANzhula <roman1985al@ukr.net>
2025-06-09 23:15:26 +0300

    Add UserInfoConsumer - user-service

---
+++ user-service/src/main/java/org/romanzhula/user_service/components/UserInfoConsumer.java
@@ -0,0 +1,21 @@
+package org.romanzhula.user_service.components;
+
+import lombok.RequiredArgsConstructor;
+import org.romanzhula.user_service.dto.events.UserInfoMessage;
+import org.romanzhula.user_service.services.UserService;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+
+@Component
+@RequiredArgsConstructor
+public class UserInfoConsumer {
+
+    private final UserService userService;
+
+    @RabbitListener(queues = "${rabbitmq.queue.name.user-info}")
+    public void consumeUserInfo(UserInfoMessage userInfoMessage) {
+        userService.saveOrUpdateUser(userInfoMessage);
+    }
+
+}
