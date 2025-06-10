dc9ec52043baf29dd14f10bf17d3c4c8c9313fac
RoMANzhula <roman1985al@ukr.net>
2025-06-10 22:09:25 +0300

    Up UserRepository - user-service - add methods find by google and github ids

--- user-service/src/main/java/org/romanzhula/user_service/repositories/UserRepository.java
+++ user-service/src/main/java/org/romanzhula/user_service/repositories/UserRepository.java
@@ -4,6 +4,13 @@ import org.romanzhula.user_service.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByGoogleId(String googleId);
+    Optional<User> findByGithubId(String githubId);
+
 }
