53312a2759e900d4cae9d2ded902a720b2a950d5
RoMANzhula <roman1985al@ukr.net>
2025-06-10 21:17:20 +0300

    Up UserService - user-service - add saveOrUpdateUser method

--- user-service/src/main/java/org/romanzhula/user_service/services/UserService.java
+++ user-service/src/main/java/org/romanzhula/user_service/services/UserService.java
@@ -2,11 +2,14 @@ package org.romanzhula.user_service.services;
 
 import org.romanzhula.user_service.dto.UserRequest;
 import org.romanzhula.user_service.dto.UserResponse;
+import org.romanzhula.user_service.dto.events.UserInfoMessage;
 
 
 public interface UserService {
 
     UserResponse getUserById(Long id);
     UserResponse saveNewUser(UserRequest userRequest);
+
+    void saveOrUpdateUser(UserInfoMessage userInfoMessage);
 
 }
