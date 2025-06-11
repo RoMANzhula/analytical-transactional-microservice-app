feed9fa249ae0da72d313d52332106638fea2ff1
RoMANzhula <roman1985al@ukr.net>
2025-06-11 22:46:31 +0300

    Add Transaction model - transaction-service

---
+++ transaction-service/src/main/java/org/romanzhula/transaction_service/models/Transaction.java
@@ -0,0 +1,41 @@
+package org.romanzhula.transaction_service.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "transactions")
+public class Transaction {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "user_id", nullable = false) // user Id from user-service
+    private Long userId;
+
+    @Column(nullable = false)
+    private BigDecimal amount;
+
+    @Column(nullable = false)
+    private String currency;
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt = LocalDateTime.now();
+
+    // optional (during testing)
+    private String googleId;
+    private String githubId;
+
+}
