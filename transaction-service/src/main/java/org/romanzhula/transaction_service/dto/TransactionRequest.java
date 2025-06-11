bbe5fcba2cbd2479d8ec7d529bb49e76b762de57
RoMANzhula <roman1985al@ukr.net>
2025-06-11 23:27:34 +0300

    Add TransactionRequest - transaction-service

---
+++ transaction-service/src/main/java/org/romanzhula/transaction_service/dto/TransactionRequest.java
@@ -0,0 +1,26 @@
+package org.romanzhula.transaction_service.dto;
+
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class TransactionRequest {
+
+    @NotNull
+    @DecimalMin(value = "0.0", inclusive = true, message = "Amount must be greater than or equal to 0")
+    private BigDecimal amount;
+
+    @NotNull
+    private String currency;
+
+}
