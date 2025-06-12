4afe5060c607a8930a35b1aa13a457b56d45dbaa
RoMANzhula <roman1985al@ukr.net>
2025-06-12 19:30:04 +0300

    Up UserController - user-service - add setPassphrase() method

--- user-service/src/main/java/org/romanzhula/user_service/controllers/UserController.java
+++ user-service/src/main/java/org/romanzhula/user_service/controllers/UserController.java
@@ -2,6 +2,7 @@ package org.romanzhula.user_service.controllers;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.romanzhula.user_service.dto.SetPassphraseRequest;
 import org.romanzhula.user_service.dto.UserRequest;
 import org.romanzhula.user_service.dto.UserResponse;
 import org.romanzhula.user_service.services.UserService;
@@ -29,6 +30,17 @@ public class UserController {
             @RequestBody @Valid UserRequest userRequest
     ) {
         return ResponseEntity.ok(userService.saveNewUser(userRequest));
+    }
+
+    // after login via OAuth2, the user must add their own secret passphrase to confirm the transaction in the future
+    // from client side we get passphrase and googleId or githubId
+    @PostMapping("/set-passphrase")
+    public ResponseEntity<String> setPassphrase(
+            @RequestBody @Valid SetPassphraseRequest request
+    ) {
+        userService.setPassphrase(request);
+
+        return ResponseEntity.ok("Passphrase set successfully!");
     }
 
 }
