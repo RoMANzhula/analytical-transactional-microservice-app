896822e9fa19900b964224f24de2aaa7ab50b188
RoMANzhula <roman1985al@ukr.net>
2025-06-13 22:31:41 +0300

    Add VerifyPassphraseRequest as dto - user-service

---
+++ user-service/src/main/java/org/romanzhula/user_service/dto/VerifyPassphraseRequest.java
@@ -0,0 +1,20 @@
+package org.romanzhula.user_service.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class VerifyPassphraseRequest {
+
+    @NotBlank
+    private Long userId;
+
+    @NotBlank
+    private String passphrase;
+
+}
