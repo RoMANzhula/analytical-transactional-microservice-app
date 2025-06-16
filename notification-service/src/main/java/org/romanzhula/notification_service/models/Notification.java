ed5ab359dbe0aae389a67a32a3c4442b8e760f18
RoMANzhula <roman1985al@ukr.net>
2025-06-16 17:17:59 +0300

    Add Notification as model - notification-service

---
+++ notification-service/src/main/java/org/romanzhula/notification_service/models/Notification.java
@@ -0,0 +1,39 @@
+package org.romanzhula.notification_service.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "notifications")
+public class Notification {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String userId;
+
+    @Column(nullable = false)
+    private String message;
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+
+
+    @PrePersist
+    protected void onCreate() {
+        createdAt = LocalDateTime.now();
+    }
+
+}
