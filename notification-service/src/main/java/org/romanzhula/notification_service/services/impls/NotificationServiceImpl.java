92861b4b29083c15a952f0544231c7df7cb6eef4
RoMANzhula <roman1985al@ukr.net>
2025-06-16 18:34:05 +0300

    Up NotificationService - notification-service - override getNotificationsByUserId() method

--- notification-service/src/main/java/org/romanzhula/notification_service/services/impls/NotificationServiceImpl.java
+++ notification-service/src/main/java/org/romanzhula/notification_service/services/impls/NotificationServiceImpl.java
@@ -9,6 +9,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -20,8 +21,9 @@ public class NotificationServiceImpl implements NotificationService {
 
     private final NotificationRepository notificationRepository;
 
-    
+
     @Override
+    @Transactional(readOnly = true)
     public Page<NotificationResponse> getAllNotifications(int page, int size) {
         Page<Notification> notificationsPage = notificationRepository.findAll(PageRequest.of(page, size));
 
@@ -34,6 +36,19 @@ public class NotificationServiceImpl implements NotificationService {
 
     }
 
+    @Override
+    @Transactional(readOnly = true)
+    public Page<NotificationResponse> getNotificationsByUserId(String userId, int page, int size) {
+        Page<Notification> notificationsPage = notificationRepository.findByUserId(userId, PageRequest.of(page, size));
+
+        List<NotificationResponse> content = notificationsPage.getContent().stream()
+                .map(this::mapToResponse)
+                .collect(Collectors.toList())
+        ;
+
+        return new PageImpl<>(content, notificationsPage.getPageable(), notificationsPage.getTotalElements());
+    }
+    
 
     private NotificationResponse mapToResponse(Notification notification) {
         return NotificationResponse.builder()
