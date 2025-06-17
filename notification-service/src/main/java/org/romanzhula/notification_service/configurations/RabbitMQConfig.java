d4d30722f6c50b805f9122855e6812b1d28f6253
RoMANzhula <roman1985al@ukr.net>
2025-06-17 22:43:48 +0300

    Add RabbitMQConfig - notification-service

---
+++ notification-service/src/main/java/org/romanzhula/notification_service/configurations/RabbitMQConfig.java
@@ -0,0 +1,62 @@
+package org.romanzhula.notification_service.configurations;
+
+import org.springframework.amqp.AmqpRejectAndDontRequeueException;
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.util.ErrorHandler;
+
+
+@Configuration
+public class RabbitMQConfig {
+
+    @Value("${spring.rabbitmq.exchange.notification.name}")
+    private String notificationExchange;
+
+    @Value("${spring.rabbitmq.queue.notification}")
+    private String notificationQueue;
+
+    @Value("${spring.rabbitmq.routing-key.notification}")
+    private String notificationRoutingKey;
+
+
+    @Bean
+    public TopicExchange notificationExchange() {
+        return new TopicExchange(notificationExchange);
+    }
+
+    @Bean
+    public Queue notificationQueue() {
+        return new Queue(notificationQueue, true);
+    }
+
+    @Bean
+    public Binding notificationBinding(
+            Queue notificationQueue,
+            TopicExchange notificationExchange
+    ) {
+        return BindingBuilder
+                .bind(notificationQueue)
+                .to(notificationExchange)
+                .with(notificationRoutingKey)
+        ;
+    }
+
+    @Bean
+    public Jackson2JsonMessageConverter jsonMessageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+
+    @Bean
+    public ErrorHandler rabbitErrorHandler() {
+        return (Throwable t) -> {
+            throw new AmqpRejectAndDontRequeueException("Error processing message", t);
+        };
+    }
+
+}
