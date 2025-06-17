fd1116185c0de879f14ced2282eaf4a22ab72227
RoMANzhula <roman1985al@ukr.net>
2025-06-17 22:41:51 +0300

    Add NotificationRequestDTO - notification-service

---
+++ notification-service/src/main/java/org/romanzhula/notification_service/dto/events/NotificationRequestDTO.java
@@ -0,0 +1,23 @@
+package org.romanzhula.notification_service.dto.events;
+
+
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class NotificationRequestDTO {
+
+    @NotNull
+    private String userId;
+
+    @NotNull
+    private String message;
+
+}
