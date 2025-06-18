6edbc8aa3af5c42d05424dba747b97cb1791ca2b
RoMANzhula <roman1985al@ukr.net>
2025-06-18 23:11:05 +0300

    Add RabbitMQConfig - analytics-service

---
+++ analytics-service/src/main/java/org/romanzhula/analytics_service/configurations/RabbitMQConfig.java
@@ -0,0 +1,53 @@
+package org.romanzhula.analytics_service.configurations;
+
+import org.springframework.amqp.AmqpRejectAndDontRequeueException;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.util.ErrorHandler;
+
+
+@Configuration
+public class RabbitMQConfig {
+
+    public static final String ANALYTICS_EXCHANGE = "analytics-exchange";
+    public static final String ANALYTICS_QUEUE = "analytics-queue";
+    public static final String ANALYTICS_ROUTING_KEY = "analytics.routing.key";
+
+
+    @Bean
+    public TopicExchange analyticsExchange() {
+        return new TopicExchange(ANALYTICS_EXCHANGE);
+    }
+
+    @Bean
+    public Queue analyticsQueue() {
+        return new Queue(ANALYTICS_QUEUE, true);
+    }
+
+    @Bean
+    public Binding analyticsBinding(Queue analyticsQueue, TopicExchange analyticsExchange) {
+        return BindingBuilder
+                .bind(analyticsQueue)
+                .to(analyticsExchange)
+                .with(ANALYTICS_ROUTING_KEY)
+        ;
+    }
+
+    @Bean
+    public Jackson2JsonMessageConverter jsonMessageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+
+    @Bean
+    public ErrorHandler rabbitErrorHandler() {
+        return t -> {
+            throw new AmqpRejectAndDontRequeueException("Error processing message", t);
+        };
+    }
+
+}
