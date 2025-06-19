a275eb395e659fbb6bdcd057d025efc73cd5ca7d
RoMANzhula <roman1985al@ukr.net>
2025-06-19 20:37:34 +0300

    Up AnalyticsRepository - analytics-service - add findAll() method

--- analytics-service/src/main/java/org/romanzhula/analytics_service/repositories/AnalyticsRepository.java
+++ analytics-service/src/main/java/org/romanzhula/analytics_service/repositories/AnalyticsRepository.java
@@ -1,13 +1,16 @@
 package org.romanzhula.analytics_service.repositories;
 
 import lombok.RequiredArgsConstructor;
+import org.romanzhula.analytics_service.dto.AnalyticsResponseDto;
 import org.romanzhula.analytics_service.models.AnalyticsRecord;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.util.List;
 
 
 @Repository
@@ -15,6 +18,7 @@ import java.sql.SQLException;
 public class AnalyticsRepository {
 
     private final DataSource dataSource; // for INSERT
+    private final JdbcTemplate jdbcTemplate; // for SELECT
 
 
     public void save(AnalyticsRecord record) {
@@ -33,5 +37,27 @@ public class AnalyticsRepository {
         }
     }
 
+    public List<AnalyticsResponseDto> findAll(int page, int size) {
+        int offset = page * size;
+        String sql = """
+                SELECT user_id, event_type, event_time
+                FROM analytics_records
+                ORDER BY event_time DESC
+                LIMIT ? OFFSET ?
+                """
+        ;
+
+        return jdbcTemplate.query(
+                sql,
+                new Object[]{size, offset},
+                (rs, rowNum) -> AnalyticsResponseDto.builder()
+                        .id(rs.getString("id"))
+                        .userId(rs.getString("user_id"))
+                        .eventType(rs.getString("event_type"))
+                        .eventTime(rs.getTimestamp("event_time").toLocalDateTime())
+                        .build()
+                )
+        ;
+    }
 
 }
