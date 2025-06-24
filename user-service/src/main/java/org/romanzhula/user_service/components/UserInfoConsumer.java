735241ab99073b50bd380a765de4c08b23b49aeb
RoMANzhula <roman1985al@ukr.net>
2025-06-24 20:08:12 +0300

    Up UserInfoConsumer - user-service - add lombok logs

--- user-service/src/main/java/org/romanzhula/user_service/components/UserInfoConsumer.java
+++ user-service/src/main/java/org/romanzhula/user_service/components/UserInfoConsumer.java
@@ -1,20 +1,24 @@
 package org.romanzhula.user_service.components;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.romanzhula.user_service.dto.events.UserInfoMessage;
 import org.romanzhula.user_service.services.UserService;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class UserInfoConsumer {
 
     private final UserService userService;
 
+    
     @RabbitListener(queues = "${spring.rabbitmq.queue.name.user-info}")
     public void consumeUserInfo(UserInfoMessage userInfoMessage) {
+        log.info("Got message: {}", userInfoMessage);
         userService.saveOrUpdateUser(userInfoMessage);
     }
 
