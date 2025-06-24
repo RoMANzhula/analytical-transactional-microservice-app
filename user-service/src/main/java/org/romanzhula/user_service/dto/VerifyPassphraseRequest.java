ffb1b94887b81da599237c7e75fc74a7b6aa1fcf
RoMANzhula <roman1985al@ukr.net>
2025-06-24 22:36:56 +0300

    Up Postman_testing.txt - user-service - check verify passphrase and fix @NotNull in request

--- user-service/src/main/java/org/romanzhula/user_service/dto/VerifyPassphraseRequest.java
+++ user-service/src/main/java/org/romanzhula/user_service/dto/VerifyPassphraseRequest.java
@@ -1,6 +1,7 @@
 package org.romanzhula.user_service.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,7 +12,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class VerifyPassphraseRequest {
 
-    @NotBlank
+    @NotNull
     private Long userId;
 
     @NotBlank
