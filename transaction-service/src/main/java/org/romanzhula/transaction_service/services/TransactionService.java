005269c62b55eac0499fe2c129b8e83933ba5e35
RoMANzhula <roman1985al@ukr.net>
2025-06-26 22:50:44 +0300

    Up TransactionService - transaction-service - add getTransactionsByUserId() method

--- transaction-service/src/main/java/org/romanzhula/transaction_service/services/TransactionService.java
+++ transaction-service/src/main/java/org/romanzhula/transaction_service/services/TransactionService.java
@@ -2,11 +2,15 @@ package org.romanzhula.transaction_service.services;
 
 import org.romanzhula.transaction_service.dto.TransactionRequest;
 import org.romanzhula.transaction_service.dto.TransactionResponse;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 
 
 public interface TransactionService {
 
     ResponseEntity<TransactionResponse> createTransaction(TransactionRequest request);
+
+    Page<TransactionResponse> getTransactionsByUserId(Long userId, Pageable pageable);
 
 }
