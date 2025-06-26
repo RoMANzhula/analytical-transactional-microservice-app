4116b3e7eb5dcdd2d19867e6030ab5aea0469196
RoMANzhula <roman1985al@ukr.net>
2025-06-26 22:42:38 +0300

    Add AppConfig - transaction-service - add model-mapper

---
+++ transaction-service/src/main/java/org/romanzhula/transaction_service/configurations/AppConfig.java
@@ -0,0 +1,16 @@
+package org.romanzhula.transaction_service.configurations;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+public class AppConfig {
+
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
+
+}
